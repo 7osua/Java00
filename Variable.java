a613d10497bc00613516993a0113366f5318f352
Josua Manullang <j7.manullang@gmail.com>
2021-12-13 01:38:30 +0700

    [+] mulai berkenalan dengan penulisan syntaks dan penulisan variabel

---
+++ Variable.java
@@ -0,0 +1,29 @@
+public class Variable {
+    public static void main(String[] args){
+        String fullName = "Josua" + " " + "Manullang";
+        char gender = 'M';
+        boolean hasJob = false;
+        boolean isBusy = true;
+        int startSalaryExpect = 4_400_000;
+        short age = 24;
+        float height = 177.7F;
+        int hexFavColor = 0x212529;
+        String currentPlace = "WMX";
+        var somethingToLog = "Java + JavaScript is Awesome !!!";
+
+        System.out.print("Variable  :\n");
+        System.out.println(fullName);
+        System.out.println(gender);
+        System.out.println(hasJob);
+        System.out.println(isBusy);
+        System.out.println(startSalaryExpect);
+        System.out.println(age);
+        System.out.println(hexFavColor);
+
+        System.out.println(currentPlace);
+        currentPlace = "YK";
+        System.out.println(currentPlace);
+
+        System.out.println(somethingToLog);
+    }
+}
