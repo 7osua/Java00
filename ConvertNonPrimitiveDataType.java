3324dcf19e707b701a99bc7d551be2135c115aa1
Josua Manullang <j7.manullang@gmail.com>
2021-12-14 22:11:15 +0700

    [^] menambah systaks untuk eksplorasi

--- ConvertNonPrimitiveDataType.java
+++ ConvertNonPrimitiveDataType.java
@@ -6,6 +6,7 @@ public class ConvertNonPrimitiveDataType {
         long initLong = 127_000_000_000L;
         float initFloat = 127_000_000.05F;
         double initDouble = 127_000_000_000.9999F;
+
         boolean initBoolean = true;
         char initChar = '7';
 
@@ -23,13 +24,28 @@ public class ConvertNonPrimitiveDataType {
         System.out.println(initByte01.doubleValue());
         System.out.println(initByte01.toString());
 
+        Double initDouble01 = initDouble;
+        System.out.println("\n" + initDouble01);
+        System.out.println(initDouble01.byteValue());
+        System.out.println(initDouble01.shortValue());
+        System.out.println(initDouble01.intValue());
+        System.out.println(initDouble01.longValue());
+        System.out.println(initDouble01.floatValue());
+        System.out.println(initDouble01.toString());
+
+
         Boolean initBoolean01 = initBoolean;
-        System.out.println(initBoolean);
+        System.out.println("\n" + initBoolean);
         System.out.println(initBoolean01);
         System.out.println(initBoolean01.toString());
 
         Character initChar01 = initChar;
-        System.out.println(initChar01);
+        System.out.println("\n" + initChar01);
         System.out.println(initChar01.toString());
+
+        String someWords = "I love Java";
+        System.out.println("\n" + someWords);
+        System.out.println(someWords.toLowerCase());
+        System.out.println(someWords.toUpperCase());
     }
 }
