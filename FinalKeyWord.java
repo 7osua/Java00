a682c11113f7311d4a54bcba3c154cc473dad543
Josua Manullang <j7.manullang@gmail.com>
2021-12-14 20:56:09 +0700

    [-] Mengubah nama file

---
+++ FinalKeyWord.java
@@ -0,0 +1,14 @@
+public class FinalKeyWord {
+    public static void main(String[] args){
+        final var fullName = "Josua Manullang";
+
+        var firstName = "Josua";
+
+        System.out.println(firstName);
+        firstName = "jos";
+
+        System.out.println(firstName);
+        System.out.println(fullName);
+
+    }
+}
